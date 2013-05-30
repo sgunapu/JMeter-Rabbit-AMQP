558155c2df6953637cc77d400a40fc1482775e12
Sridhar Gunapu <sgunapu@gmail.com>
2013-05-30 00:12:03 -0700

    added text file, random file, text message options

--- src/main/com/zeroclue/jmeter/protocol/amqp/AMQPPublisher.java
+++ src/main/com/zeroclue/jmeter/protocol/amqp/AMQPPublisher.java
@@ -2,11 +2,17 @@ package com.zeroclue.jmeter.protocol.amqp;
 
 import com.rabbitmq.client.AMQP;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.rabbitmq.client.MessageProperties;
+import com.zeroclue.jmeter.protocol.amqp.gui.AMQPPublisherGui;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.Interruptible;
 import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.services.FileServer;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.io.TextFile;
 import org.apache.jorphan.logging.LoggingManager;
 import org.apache.log.Logger;
 
@@ -40,6 +46,15 @@ public class AMQPPublisher extends AMQPSampler implements Interruptible {
 
     public static boolean DEFAULT_USE_TX = false;
     private final static String USE_TX = "AMQPConsumer.UseTx";
+
+    private static final FileServer FSERVER = FileServer.getFileServer();
+    private static final String MESSAGE_SOURCE = "AMQPConsumer.MessageSource";
+    private static final String INPUT_FILE = "AMQPConsumer.InputFile";
+    private static final String RANDOM_PATH = "AMQPConsumer.RandomPath";
+    public static final String DEFAULT_MESSAGE_SOURCE = AMQPPublisherGui.USE_TEXT_RSC;
+
+    // Cache for bytes-message, only used when parsing from a file
+    private Map<String, String> fileCache = new HashMap<String, String>();
 
     private transient Channel channel;
 
@@ -117,7 +132,7 @@ public class AMQPPublisher extends AMQPSampler implements Interruptible {
 
 
     private byte[] getMessageBytes() {
-        return getMessage().getBytes();
+        return getMessageContent().getBytes();
     }
 
     /**
@@ -192,6 +207,41 @@ public class AMQPPublisher extends AMQPSampler implements Interruptible {
        setProperty(USE_TX, tx);
     }
 
+    /**
+     * set the input file for the publisher
+     *
+     * @param file
+     */
+    public void setInputFile(String file) {
+        setProperty(INPUT_FILE, file);
+    }
+
+    /**
+     * return the path of the input file
+     *
+     */
+    public String getInputFile() {
+        return getPropertyAsString(INPUT_FILE);
+    }
+
+    /**
+     * set the random path for the messages
+     *
+     * @param path
+     */
+    public void setRandomPath(String path) {
+        setProperty(RANDOM_PATH, path);
+    }
+
+    /**
+     * return the random path for messages
+     *
+     */
+    public String getRandomPath() {
+        return getPropertyAsString(RANDOM_PATH);
+    }
+
+
     @Override
     public boolean interrupt() {
         cleanup();
@@ -231,4 +281,58 @@ public class AMQPPublisher extends AMQPSampler implements Interruptible {
         }
         return ret;
     }
+
+    /**
+     * Method will check the setting and get the contents for the message.
+     *
+     * @return the contents for the message
+     */
+    private String getMessageContent() {
+        if (getMessageSource().equals(AMQPPublisherGui.USE_FILE_RSC)) {
+            return getFileContent(getInputFile());
+        } else if (getMessageSource().equals(AMQPPublisherGui.USE_RANDOM_RSC)) {
+            // Maybe we should consider creating a global cache for the
+            // random files to make JMeter more efficient.
+            String fname = FSERVER.getRandomFile(getRandomPath(), new String[] { ".txt" })
+                    .getAbsolutePath();
+            return getFileContent(fname);
+        } else {
+            return getMessage();
+        }
+    }
+
+    /**
+     * The implementation uses TextFile to load the contents of the file and
+     * returns a string.
+     *
+     * @param path
+     * @return the contents of the file
+     */
+    protected String getFileContent(String path) {
+        String fileContent = fileCache.get(path);
+        if (fileContent == null) {
+            TextFile tf = new TextFile(path);
+            fileContent = tf.getText();
+        }
+        return fileContent;
+    }
+
+    /**
+     * set the source of the message
+     *
+     * @param messageSource
+     */
+    public void setMessageSource(String messageSource) {
+        setProperty(MESSAGE_SOURCE, messageSource);
+    }
+
+    /**
+     * return the source of the message
+     * Converts from old JMX files which used the local language string
+     */
+    public String getMessageSource() {
+        return getPropertyAsString(MESSAGE_SOURCE, DEFAULT_MESSAGE_SOURCE);
+    }
+
+
 }
--- src/main/com/zeroclue/jmeter/protocol/amqp/gui/AMQPPublisherGui.java
+++ src/main/com/zeroclue/jmeter/protocol/amqp/gui/AMQPPublisherGui.java
@@ -3,7 +3,11 @@ package com.zeroclue.jmeter.protocol.amqp.gui;
 import java.awt.Dimension;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
+import org.apache.jmeter.gui.util.FilePanel;
+import org.apache.jmeter.gui.util.JLabeledRadioI18N;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jorphan.gui.JLabeledTextArea;
 import org.apache.jorphan.gui.JLabeledTextField;
@@ -20,18 +24,20 @@ import com.zeroclue.jmeter.protocol.amqp.AMQPPublisher;
  * additional setup that a test would need at run-time
  *
  */
-public class AMQPPublisherGui extends AMQPSamplerGui {
+public class AMQPPublisherGui extends AMQPSamplerGui implements ChangeListener {
 
     private static final long serialVersionUID = 1L;
 
+    private static final String ALL_FILES = "*.*"; //$NON-NLS-1$
+    /** Take source from the named file */
+    public static final String USE_FILE_RSC   = "jms_use_file"; //$NON-NLS-1$
+    /** Take source from a random file */
+    public static final String USE_RANDOM_RSC = "jms_use_random_file"; //$NON-NLS-1$
+    /** Take source from the text area */
+    public static final String USE_TEXT_RSC   = "jms_use_text"; //$NON-NLS-1$
+
     private JPanel mainPanel;
 
-    /*
-    private static final String[] CONFIG_CHOICES = {"File", "Static"};
-    private final JLabeledRadio configChoice = new JLabeledRadio("Message Source", CONFIG_CHOICES);
-    private final FilePanel messageFile = new FilePanel("Filename", ALL_FILES);
-    */
-    private JLabeledTextArea message = new JLabeledTextArea("Message Content");
     private JLabeledTextField messageRoutingKey = new JLabeledTextField("Routing Key");
     private JLabeledTextField messageType = new JLabeledTextField("Message Type");
     private JLabeledTextField replyToQueue = new JLabeledTextField("Reply-To Queue");
@@ -39,6 +45,14 @@ public class AMQPPublisherGui extends AMQPSamplerGui {
 
     private JCheckBox persistent = new JCheckBox("Persistent?", AMQPPublisher.DEFAULT_PERSISTENT);
     private JCheckBox useTx = new JCheckBox("Use Transactions?", AMQPPublisher.DEFAULT_USE_TX);
+
+    private final FilePanel messageFile = new FilePanel("Message File", ALL_FILES);
+    private final FilePanel randomFile = new FilePanel("Random File", ALL_FILES);
+    private final JLabeledTextArea textMessage = new JLabeledTextArea("Text Message");
+
+ // Button group resources
+    private static final String[] MESSAGE_SOURCE_ITEMS = { USE_FILE_RSC, USE_RANDOM_RSC, USE_TEXT_RSC };
+    private final JLabeledRadioI18N messageSource = new JLabeledRadioI18N("jms_config", MESSAGE_SOURCE_ITEMS, USE_TEXT_RSC); //$NON-NLS-1$
 
     public AMQPPublisherGui(){
         init();
@@ -73,7 +87,13 @@ public class AMQPPublisherGui extends AMQPSamplerGui {
         messageType.setText(sampler.getMessageType());
         replyToQueue.setText(sampler.getReplyToQueue());
         correlationId.setText(sampler.getCorrelationId());
-        message.setText(sampler.getMessage());
+
+        updateMessageSource(sampler.getMessageSource());
+        messageFile.setFilename(sampler.getInputFile());
+        randomFile.setFilename(sampler.getRandomPath());
+        textMessage.setText(sampler.getMessage());
+
+        updateMessageSource(sampler.getMessageSource());
     }
 
     /**
@@ -101,10 +121,15 @@ public class AMQPPublisherGui extends AMQPSamplerGui {
         sampler.setUseTx(useTx.isSelected());
 
         sampler.setMessageRoutingKey(messageRoutingKey.getText());
-        sampler.setMessage(message.getText());
         sampler.setMessageType(messageType.getText());
         sampler.setReplyToQueue(replyToQueue.getText());
         sampler.setCorrelationId(correlationId.getText());
+
+        sampler.setMessage(textMessage.getText());
+        sampler.setInputFile(messageFile.getFilename());
+        sampler.setRandomPath(randomFile.getFilename());
+
+        sampler.setMessageSource(messageSource.getText());
     }
 
     @Override
@@ -124,7 +149,6 @@ public class AMQPPublisherGui extends AMQPSamplerGui {
         messageType.setPreferredSize(new Dimension(100, 25));
         replyToQueue.setPreferredSize(new Dimension(100, 25));
         correlationId.setPreferredSize(new Dimension(100, 25));
-        message.setPreferredSize(new Dimension(400, 150));
 
         mainPanel.add(persistent);
         mainPanel.add(useTx);
@@ -132,7 +156,19 @@ public class AMQPPublisherGui extends AMQPSamplerGui {
         mainPanel.add(messageType);
         mainPanel.add(replyToQueue);
         mainPanel.add(correlationId);
-        mainPanel.add(message);
+
+        messageSource.setLayout(new BoxLayout(messageSource, BoxLayout.X_AXIS));
+        messageSource.setText(AMQPPublisher.DEFAULT_MESSAGE_SOURCE);
+        mainPanel.add(messageSource);
+        messageSource.addChangeListener(this);
+
+        mainPanel.add(messageFile);
+        mainPanel.add(randomFile);
+        mainPanel.add(textMessage);
+        Dimension pref = new Dimension(400, 150);
+        textMessage.setPreferredSize(pref);
+
+        updateMessageSource(messageSource.getText());
     }
 
     /**
@@ -147,6 +183,45 @@ public class AMQPPublisherGui extends AMQPSamplerGui {
         messageType.setText("");
         replyToQueue.setText("");
         correlationId.setText("");
-        message.setText("");
+
+        textMessage.setText("");
+        messageFile.setFilename("");
+        randomFile.setFilename("");
+        textMessage.setText("");
+
+        updateMessageSource(AMQPPublisher.DEFAULT_MESSAGE_SOURCE);
     }
+
+    /**
+     * Update config contains the actual logic for enabling or disabling text
+     * message, file or random path.
+     *
+     * @param command
+     */
+    private void updateMessageSource(String command) {
+        messageSource.setText(command);
+        if (command.equals(USE_TEXT_RSC)) {
+            textMessage.setVisible(true);
+            messageFile.setVisible(false);
+            randomFile.setVisible(false);
+        } else if (command.equals(USE_RANDOM_RSC)) {
+            textMessage.setVisible(false);
+            messageFile.setVisible(false);
+            randomFile.setVisible(true);
+        } else if (command.equals(USE_FILE_RSC)) {
+            textMessage.setVisible(false);
+            messageFile.setVisible(true);
+            randomFile.setVisible(false);
+        }
+    }
+
+
+    @Override
+    public void stateChanged(ChangeEvent event) {
+        if (event.getSource() == messageSource) {
+            updateMessageSource(messageSource.getText());
+        }
+    }
+
+
 }
